08e73822dfe759f4a299deba8e0f3209d4fdb945
jangilirohini <144904990+jangilirohini@users.noreply.github.com>
2024-04-05 08:29:14 +0530

    Add files via upload

---
+++ project_CollegeManagement/src/main/java/com/example/demo/repository/CollegeRepo.java
@@ -0,0 +1,17 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.College;
+
+
+
+
+public interface CollegeRepo extends JpaRepository<College, Long>  {
+
+	
+	
+    
+	
+	
+}
